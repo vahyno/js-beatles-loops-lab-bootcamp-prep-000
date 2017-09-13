33e2f1657ccf3f72debbec90b8f3c70df7968e5b
M V <vahyno+learn@gmail.com>
2017-09-13 06:59:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function iLoveTheBeatles(number){
   var emptyArray = []
   do {
     emptyArray.push("I love the Beatles!");
-    ++number;
+    number++;
   }
   while (number<15);
   return emptyArray;
